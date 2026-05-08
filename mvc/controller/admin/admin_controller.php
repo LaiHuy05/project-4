29a3eac5c4dd1fcd3a4e1b18e84a9b2df5c8b307
HUY <huy20050416@gmail.com>
2026-05-08 12:12:00 +0700

    update api and controller

--- mvc/controller/admin/admin_controller.php
+++ mvc/controller/admin/admin_controller.php
@@ -361,113 +361,6 @@ switch ($admin) {
         }
         include "view/admin/product/update.php";
         break;
-        $thongBao = "";
-        $thongBaoLoi = "";
-        $id = $_GET['id'] ?? '';
-        $list_product = load_all_product();
-        $list_category = load_all_category();
-        $list_product_color = load_all_product_color();
-        $list_product_memory = load_all_product_memory();
-
-
-        $thongBaoLoiTen = '';
-        $thongBaoLoiAnh = '';
-        $thongBaoLoiGia = '';
-        $thongBaoLoiSoLuong = '';
-        $thongBaoLoiMoTa = '';
-        $thongBaoLoiDM = '';
-        $thongBaoLoiTenBN = '';
-        $thongBaoLoiTenMau = '';
-        $thongBaoMau = '';
-        $thongBaoBN = '';
-
-        $product = load_one_product($id);
-        $sp_id = $product['sp_id'];
-        $sp_name = $product['sp_name'];
-        $sp_image = $product['sp_image'];
-        $sp_price = $product['sp_price'];
-        $sp_quantity = $product['sp_quantity'];
-        $sp_describe = $product['sp_describe'];
-        $id_dm = $product['id_dm'];
-        $sp_pricedel = $product['sp_pricedel'];
-
-        // $name = load_one_product($id);
-        if (isset($_POST["submit"])) {
-            $name = trim($_POST['name']);
-            $image = trim($_POST["image"]);
-            $price = trim($_POST["price"]);
-            $quantity = trim($_POST["quantity"]);
-            $describe = trim($_POST['describe']);
-            $id_dm = trim($_POST['dm_id']);
-            $sp_pricedel = trim($_POST['sp_pricedel']);
-
-            if ($name === '') {
-                $thongBaoLoiTen = 'Vui lòng nhập Tên!';
-            } elseif (strlen($name) < 5) {
-                $thongBaoLoiTen = 'Tên đăng nhập phải có ít nhất 5 ký tự.';
-            }
-
-            if ($image === '') {
-                $thongBaoLoiAnh = 'Vui lòng nhập đường dẫn ảnh!';
-            }
-
-            if ($price === '') {
-                $thongBaoLoiGia = 'Vui lòng nhập Giá!';
-            } elseif (!is_numeric($price)) {
-                $thongBaoLoiGia = 'Giá phải là một số.';
-            }
-
-            if ($quantity === '') {
-                $thongBaoLoiSoLuong = 'Vui lòng nhập Số Lượng!';
-            }
-
-            if ($describe === '') {
-                $thongBaoLoiMoTa = 'Vui lòng nhập Mô tả!';
-            }
-            if ($id_dm === '' || $id_dm === '0') {
-                $thongBaoLoiDM = 'Vui lòng chọn danh mục!';
-            }
-            if (
-                empty($thongBaoLoiTen) && empty($thongBaoLoiAnh) && empty($thongBaoLoiGia) &&
-                empty($thongBaoLoiTieuDe) && empty($thongBaoLoiSoLuong) &&
-                empty($thongBaoLoiMoTa) && empty($thongBaoLoiDM)
-            ) {
-                update_product($sp_id, $name, $image, $price, $quantity, $describe, $id_dm, $sp_pricedel);
-                $thongBao = "Sửa thành công";
-            }
-        }
-        if (isset($_POST["submit_color"])) {
-            $name = trim($_POST['name_color']);
-
-            $id_sp = $_GET["id"];
-
-            if ($name === '') {
-                $thongBaoLoiTenMau = 'Vui lòng nhập Tên!';
-            }
-            if (
-                empty($thongBaoLoiTenMau) &&  empty($thongBaoLoiSP)
-            ) {
-                insert_product_color($name, $id_sp);
-                $thongBaoMau = "Thêm thành công";
-            }
-        }
-        if (isset($_POST["submit_memory"])) {
-            $name = trim($_POST['name_memory']);
-
-            $id_sp = $_GET["id"];
-
-            if ($name === '') {
-                $thongBaoLoiTenBN = 'Vui lòng nhập Tên!';
-            }
-            if (
-                empty($thongBaoLoiTenBN) &&  empty($thongBaoLoiSP)
-            ) {
-                insert_product_memory($name, $id_sp);
-                $thongBaoBN = "Thêm thành công";
-            }
-        }
-        include "view/admin/product/update.php";
-        break;
     case 'productDelete':
         delete_product($id);
 
@@ -508,7 +401,7 @@ switch ($admin) {
         $list_category = load_all_category();
         include 'product/product-color/list.php';
         break;
-    case 'view/admin/productColor-Add':
+    case 'productColor-Add':
         $thongBao = "";
         $thongBaoLoiTen = '';
         $thongBaoLoiSP = '';
